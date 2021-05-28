0eb47a921a616f2bea07147ff0dbb90112e88db0
PragyaMaroti <64036955+PragyaMaroti@users.noreply.github.com>
2021-05-29 00:00:30 +0530

    Create code_2.cpp

---
+++ code_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x, y;
+  cin>>x;
+  cin>>y;
+  cout<<"multiplication is<< x*y;
+    
+    
+    
+    // while taking input, if user prints enter then next line is printed after taking input
+    // buy if it presses " " and then enter anotheer input, it will take both the inputs as per the required input here, x&y;
+    // this is called buffering. "processor, the fast device and keyboard the slow device accomodate through buffering"
+    
+    
+    // difference between endl and \n : Both endl and \n serve the same purpose in C++ – they insert a new line. However, the key difference between them is that endl causes a flushing of the output buffer every time it is called, whereas \n does not. 
